a0b9125c800b3f3d0f68857fe181ebff299d4f1d
Rahatkap <rahatkap2308@gmail.com>
2024-06-25 11:42:42 -0700

    data validation final

--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -11,22 +11,18 @@ sys.path.insert(0, str(src_dir))
 
 from textSummarizer.constants import *
 from textSummarizer.utils.common import read_yaml, create_directories
-from textSummarizer.entity import (DataIngestionConfig,DataValidationConfig)
-                                   
-
+from textSummarizer.entity import DataIngestionConfig, DataValidationConfig
 
 class ConfigurationManager:
     def __init__(
         self,
-        config_filepath = CONFIG_FILE_PATH,
-        params_filepath = PARAMS_FILE_PATH):
-
+        config_filepath=CONFIG_FILE_PATH,
+        params_filepath=PARAMS_FILE_PATH
+    ):
         self.config = read_yaml(config_filepath)
         self.params = read_yaml(params_filepath)
 
         create_directories([self.config.artifacts_root])
-
-    
 
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         config = self.config.data_ingestion
@@ -37,12 +33,12 @@ class ConfigurationManager:
             root_dir=config.root_dir,
             source_URL=config.source_URL,
             local_data_file=config.local_data_file,
-            unzip_dir=config.unzip_dir 
+            unzip_dir=config.unzip_dir
         )
 
         return data_ingestion_config
-    
-def get_data_validation_config(self) -> DataValidationConfig:
+
+    def get_data_validation_config(self) -> DataValidationConfig:
         config = self.config.data_validation
 
         create_directories([config.root_dir])
